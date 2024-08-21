0c88e492b96264a8f6a912b38e3192398eef3c34
Sulaim <sulaim@Sulaims-Air.local>
2024-08-21 14:32:58 +0530

    TwoSum

---
+++ TwoSum.js
@@ -0,0 +1,16 @@
+const map = {}
+const res = []
+nums = [2,7,11,15]
+let target = 9
+for(let i =0; i< nums.length ; i++){
+  console.log(map[nums[i]])
+  if(map[nums[i]] !== undefined){
+      console.log('okay')
+      res.push(map[nums[i]])
+      res.push(i)
+  }else{
+      map[target - nums[i]] = i
+  }
+}
+console.log(map)
+console.log(res)
